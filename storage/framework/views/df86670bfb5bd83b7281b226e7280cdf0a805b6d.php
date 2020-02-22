4db223eca379b7fad667b3403c336046cbbb6fc3
ahmedhegazee <ahmedhegazee@hotmail.com>
2020-02-22 21:30:53 +0200

    solve cannot add paragraphs to reading exam

--- storage/framework/views/df86670bfb5bd83b7281b226e7280cdf0a805b6d.php
+++ storage/framework/views/df86670bfb5bd83b7281b226e7280cdf0a805b6d.php
@@ -33,15 +33,16 @@
 
 
         <display-questions-panel
-            exams="<?php echo e($paragraphs); ?>"
+
             route="<?php echo e(route('paragraph.store')); ?>"
             store-route="<?php echo e(route('reading.exam.paragraph.store',compact('exam'))); ?>"
+            delete-route="<?php echo e(route('reading.exam.paragraph.store',compact('exam'))); ?>"
             is-paragraph=true
             can-choose=true
-            checked="<?php echo e($checked); ?>"
+
             redirect-route="<?php echo e(route('reading.exam.show.paragraphs',compact('exam'))); ?>"
         ></display-questions-panel>
     </div>
 <?php $__env->stopSection(); ?>
 
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/ahmedhegazy/Desktop/toefl/toeflsystem/resources/views/reading/exams/addparagraphs.blade.php ENDPATH**/ ?>
+<?php echo $__env->make('cpanel', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/ahmedhegazy/Desktop/toefl/toeflsystem/resources/views/reading/exams/addparagraphs.blade.php ENDPATH**/ ?>
